54d8f65458118822410c0681fcf78d65d363f313
dwoichenkoDmitry <66391940+dwoichenkoDmitry@users.noreply.github.com>
2021-05-11 04:56:16 +0500

    Add files via upload

---
+++ Game_space_arcanoid/Game_space_arcanoid/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Game_space_arcanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BallImg = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BallImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.BallImg);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // BallImg
+            // 
+            this.BallImg.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BallImg.BackgroundImage")));
+            this.BallImg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BallImg.Location = new System.Drawing.Point(343, 120);
+            this.BallImg.Name = "BallImg";
+            this.BallImg.Size = new System.Drawing.Size(32, 31);
+            this.BallImg.TabIndex = 0;
+            this.BallImg.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BallImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox BallImg;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Game_space_arcanoid/Game_space_arcanoid/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_space_arcanoid
+{
+    public partial class Form1 : Form
+    {
+        
+        double sx = 17, sy = 12, bx, by, sin = 1, cos = 0;
+
+
+
+        float LX, LY;
+        bool BallIsBottom = true;
+
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                cos += 0.02;
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                sin -= 0.02;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+            bx = BallImg.Left;
+            by = BallImg.Top;
+            LX = panel1.Width / 2;
+            LY = panel1.Height - 120;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double radius = (panel1.Height - 120);
+            LX = (float)((panel1.Width / 2) + radius * Math.Cos(cos));
+            LY = (float)((panel1.Height) - radius * Math.Sin(sin));
+            bx += sx;
+            by += sy;
+
+            BallImg.Left = (int)bx;
+            BallImg.Top = (int)by;
+
+
+            if ((BallImg.Top + BallImg.Height >= panel1.Height)||(BallImg.Top <= 0))
+            {
+                sy *= -1;
+            }
+            if ((BallImg.Right >= panel1.Width)||(BallImg.Left <= 0))
+            {
+                sx *= -1;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+
+
+            
+            g.DrawLine(new Pen(Color.Black, 2), panel1.Width/2, panel1.Height, panel1.Width/2, panel1.Height-120);
+            
+        }
+    }
+}
